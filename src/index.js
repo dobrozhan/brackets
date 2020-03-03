17c573de2e8c6200ae0e686ef9b5d27c427742cd
keep_chr <dobrozhan.a@gmail.com>
2020-03-04 01:27:41 +0200

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  bracketsConfig = bracketsConfig.flat().sort().join('');
+  var strArr = [];
+  var pair = '';
+  let strPush = '';
+
+  for ( let i = 0; i < str.length-1; i++ ) {
+
+      for ( let j = 0; j < bracketsConfig.length-1; j++ ) {
+
+        if ( str[i] == bracketsConfig[j] && bracketsConfig.indexOf(bracketsConfig[j]) % 2 == 0 ) {
+          pair = bracketsConfig[bracketsConfig.indexOf(bracketsConfig[j])+1];
+          strPush = str[i] + pair;
+          strArr.push(strPush);
+        }
+      }
+    }
+
+    strArr = strArr.sort().join('');
+
+    if ( strArr.includes(bracketsConfig) && strArr.length == str.length ) {
+      return true;
+    } else {
+      return false;
+    }
+};
